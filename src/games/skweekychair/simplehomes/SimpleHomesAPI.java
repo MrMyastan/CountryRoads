5e1cfcc96b1f3cba4fb88027f1eea1715da7a428
MrMyastan <53984236+MrMyastan@users.noreply.github.com>
2021-03-24 02:12:42 -0500

    moar api

--- src/games/skweekychair/simplehomes/SimpleHomesAPI.java
+++ src/games/skweekychair/simplehomes/SimpleHomesAPI.java
@@ -1,9 +1,78 @@
 package games.skweekychair.simplehomes;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
-public interface SimpleHomesAPI {
-    void changeNumHomes(UUID uuid, int change);
+import org.bukkit.Location;
 
-    int getNumHomes(UUID uuid);
+/**
+ * The API class through which you can interact with my glorious, definitely
+ * not terrible, plugin
+ */
+public interface SimpleHomesAPI {
+    /**
+     * Adds [change] number of homes to the player specified by 
+     * uuid. It is possible for a player to have a negative number of homes 
+     * remaining in which case they can only replace homes or remove them 
+     * until they have spare homes. 
+     * @param uuid 
+     * The UUID of the player to apply the change to
+     * @param change
+     * The number of homes to add, can be negative to remove homes
+     * @return
+     * true if the change succeeded, false if the player's data could 
+     * not be found
+     */
+    boolean changeHomesRemaining(UUID uuid, int change);
+
+    /**
+     * Gets the number of homes remaining for a player
+     * @param uuid
+     * The UUID of the player
+     * @return
+     * The number of homes remaining or null if the player's data can't be 
+     * found. If the player has not added a home before it won't be in the 
+     * store and will return null.
+     */
+    Integer getHomesRemaining(UUID uuid);
+
+    /**
+     * Sets the remaining homes for a player
+     * @param uuid
+     * The UUID of the player
+     * @param homes
+     * What to set their remaining homes to
+     * @return
+     * true on success, false if the specified player's data couldn't be found
+     */
+    boolean setHomesRemaining(UUID uuid, int homes);
+
+    /**
+     * Gets the homes of a player
+     * @param uuid
+     * The UUID of the player
+     * @return
+     * A map of home names to their locations or null if the player's data 
+     * could not be found
+     */
+    Map<String, Location> getUserHomes(UUID uuid);
+
+    /**
+     * Gets all of the UUIDs we are storing data for
+     * @return
+     * A set of player UUIDs
+     */
+    Set<UUID> getRegisteredUUIDS();
+
+    /**
+     * Gets the initial number of homes given to a player
+     * <p>
+     * The homes are allocated when they first create a home (also when they 
+     * are first entered into the data store)
+     * @return
+     * The initial number of homes, 0 or less disables home limiting
+     */
+    int getInitialHomes();
+
 }
--- src/games/skweekychair/simplehomes/SimpleHomesAPIImpl.java
+++ src/games/skweekychair/simplehomes/SimpleHomesAPIImpl.java
@@ -1,7 +1,12 @@
 package games.skweekychair.simplehomes;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
+import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class SimpleHomesAPIImpl implements SimpleHomesAPI {
@@ -15,16 +20,71 @@ public class SimpleHomesAPIImpl implements SimpleHomesAPI {
     }
 
     @Override
-    public void changeNumHomes(UUID uuid, int change) {
+    public boolean changeHomesRemaining(UUID uuid, int change) {
+        if (!users.isConfigurationSection(uuid.toString())) {
+            return false;
+        }
+        
         // is this gonna be a race condition?
         String remainingPath = uuid + ".homes-remaining";
         users.set(remainingPath, users.getInt(remainingPath) + change);
         plugin.saveConfig();
+        return true;
     }
 
     @Override
-    public int getNumHomes(UUID uuid) {
-        return users.getInt(uuid + ".homes-remaining");
+    public Integer getHomesRemaining(UUID uuid) {
+        if (!users.isInt(uuid + ".homes-remaining")) {
+            return null;
+        }
+        
+        return Integer.valueOf(users.getInt(uuid + ".homes-remaining"));
+    }
+
+    @Override
+    public boolean setHomesRemaining(UUID uuid, int homes) {
+        if (!users.isConfigurationSection(uuid.toString())) {
+            return false;
+        }
+        
+        users.set(uuid + ".homes-remaining", homes);
+        plugin.saveConfig();
+        return true;
+    }
+
+    @Override
+    public Map<String, Location> getUserHomes(UUID uuid) {
+        if (!users.isConfigurationSection(uuid.toString())) {
+            return null;
+        }
+        
+        HashMap<String, Location> homes = new HashMap<String, Location>();
+        ConfigurationSection homesSection = users.getConfigurationSection(uuid + ".homes");
+
+        for (String key : homesSection.getKeys(false)) {
+            // I'm ignoring invalid locations, should i be doing that?
+            if (homesSection.isLocation(key)) {
+                homes.put(key, homesSection.getLocation(key));
+            }
+        }
+
+        return homes;
+    }
+
+    @Override
+    public Set<UUID> getRegisteredUUIDS() {
+        Set<UUID> uuids = new HashSet<UUID>();
+
+        for (String key : users.getKeys(false)) {
+            uuids.add(UUID.fromString(key));
+        }
+        
+        return uuids;
+    }
+
+    @Override
+    public int getInitialHomes() {
+        return users.getParent().getInt("initial-homes");
     }
 
 }
